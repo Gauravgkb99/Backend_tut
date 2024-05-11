68d5ad37e6d4ccab23c2a129c278b2ba1c545922
Gauravgkb99 <gauravkingbudhlakoti9999@gmail.com>
2024-05-12 03:12:13 +0530

    Adding a database to our program

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+import { DB_NAME } from '../constants.js'
+import express from 'express'
+
+const app = express() 
+
+const connectDB = async () => {                  
+    try{                        
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        
+        console.log(`\n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`)
+    }
+    catch(error){
+        console.error("MongoDB connection error", error)
+        process.exit(1)
+        // throw err
+    }                         
+}
+
+export default connectDB
