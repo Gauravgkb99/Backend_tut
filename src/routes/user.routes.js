5634467b73184028a7047e9a823bb196f88df0dc
Gauravgkb99 <gauravkingbudhlakoti9999@gmail.com>
2024-05-23 18:56:48 +0530

    added routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -18,6 +18,13 @@ router.route('/login').post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJwt, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt, changeCurrentPassword)
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+router.route("/update-details").patch(verifyJwt, updateAccountDetails)
+router.route("/avatar").patch(verifyJwt, upload.single('avatar'), updateUserAvatar)
+router.route("/cover-image").patch(verifyJwt, upload.single('coverImage'), updateUserCoverImage)
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile)
+router.route("/history").get(verifyJwt, getWatchHistory)
 
 // pehle app.js mein call hogi and then wo register ke liye redirect hogi yahan toh 
 // toh jo hmara address banega wo banega iss type se https://localhost:8000/api/v1/user/register
